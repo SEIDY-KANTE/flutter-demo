9a1964c64f5845d5e2812ce758f553e833c420e5
SEIDY-KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2023-06-06 00:33:55 +0300

    create button

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
+
   runApp(MaterialApp(
     home: Scaffold(
       appBar: AppBar(
         title: const Text("My flutter App"),
+      ),
+      body: Center(
+        child: ElevatedButton(onPressed: (){
+          var alert=const AlertDialog(
+            title: Text("Result"),
+            content: Text("Super"),
+          );
+        }, child: const Text('Press Button')),
       ),
     ),
   ));
