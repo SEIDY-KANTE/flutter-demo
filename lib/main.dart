656bdb58d2e1b860452a1105c6a6858435f7335f
SEIDY-KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2023-06-06 00:56:52 +0300

    creating of functions and column

--- lib/main.dart
+++ lib/main.dart
@@ -11,23 +11,52 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var students = ["student1", "student2", "student3", "student4"];
     return Scaffold(
       appBar: AppBar(
         title: const Text("My flutter App"),
       ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              var alert = const AlertDialog(
-                title: Text("Result"),
-                content: Text("Super"),
-              );
-
-              showDialog(
-                  context: context, builder: (BuildContext context) => alert);
-            },
-            child: const Text('Press Button')),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+                itemCount: students.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Text(students[index]);
+                }),
+          ),
+          Center(
+            child: ElevatedButton(
+                onPressed: () {
+                  //functions call
+                  String msg = statusMessage(55);
+                  displayMessage(context, msg);
+                },
+                child: const Text('Press Button')),
+          ),
+        ],
       ),
     );
+  }
+
+  //Functions
+  String statusMessage(int not) {
+    String msg = '';
+    if (not >= 50) {
+      msg = "High Level";
+    } else if (not >= 40) {
+      msg = "Middle Level";
+    } else {
+      msg = "Low Level";
+    }
+    return msg;
+  }
+
+  void displayMessage(BuildContext context, msg) {
+    var alert = AlertDialog(
+      title: const Text("Result"),
+      content: Text(msg),
+    );
+    showDialog(context: context, builder: (BuildContext context) => alert);
   }
 }
