f607f8b18953c70e611b877e074787340749b692
SEIDY-KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2023-06-06 00:39:52 +0300

    first class

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(const MaterialApp(
+    home: MyApp(),
+  ));
+}
 
-  runApp(MaterialApp(
-    home: Scaffold(
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: const Text("My flutter App"),
       ),
       body: Center(
-        child: ElevatedButton(onPressed: (){
-          var alert=const AlertDialog(
-            title: Text("Result"),
-            content: Text("Super"),
-          );
-        }, child: const Text('Press Button')),
+        child: ElevatedButton(
+            onPressed: () {
+              var alert = const AlertDialog(
+                title: Text("Result"),
+                content: Text("Super"),
+              );
+
+              showDialog(
+                  context: context, builder: (BuildContext context) => alert);
+            },
+            child: const Text('Press Button')),
       ),
-    ),
-  ));
+    );
+  }
 }
